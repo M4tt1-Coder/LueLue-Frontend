edb0519516e27c29c5659eaa0eaa8dcd7774d946
M4tt1-Coder <geimat75@gmail.com>
2025-04-29 11:25:00 +0200

    [ Main ] CI/CD Pipeline integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+// TODO - Connect to cloudflare pages -> https://developers.cloudflare.com/pages/framework-guides/nextjs/
+// TODO - Set up CI / CD -> connect to Cloudflare workers with GitHub Actions
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
